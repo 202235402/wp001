183ce8dd6cb1d07199417854da2bd0dd03ccd0cc
minju kang <kmj20030421@gmail.com>
2024-04-12 20:01:22 +0900

    2

--- app/main.py
+++ app/main.py
@@ -10,16 +10,15 @@ def read_name():
     return {"name": user_name}
 
 @app.post("/")
-def create_name():
-
-    return {}
+def create_name(new_name: str, q: Union[str, None] = None):
+    return {"name": new_name}
 
 @app.put("/")
-def update_name():
-
-    return {}
+def update_name(updated_name: str, q: Union[str, None] = None):
+    user_name = updated_name
+    return {"name": user_name, "q": q}
 
 @app.delete("/")
 def delete_name():
     user_name = ""
-    return {"nme": "has been deleted"}
+    return {"name": "has been deleted"}
