d129b632bef40e2e6bb3c21066f10d919747d138
minju kang <kmj20030421@gmail.com>
2024-04-12 17:21:11 +0900

    init

--- app/main.py
+++ app/main.py
@@ -3,22 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-user_id = "minju"
+user_name = "minju"
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, q: str = None):
-    return {"user_id": user_id, "q": q}
-
-
-@app.post("/users/")
-def create_user(q: str = None):
-    return {"q": q}
-
-@app.put("/users/{user_id}")
-def update_user(user_id: int, q: str = None):
-    return {"user_id": user_id, "q": q}
-
-@app.delete("/users/")
-def delete_user():
-    user_id = ""
-    return {"message": "User has been deleted"}
+@app.get("/")
+def read_name():
+    return {"name": user_name}
