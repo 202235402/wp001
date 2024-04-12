06714cf9167a513b1b285677eae3ef82abd59c26
minju kang <kmj20030421@gmail.com>
2024-04-12 17:19:56 +0900

    test:

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from typing import Union
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,18 +6,19 @@ app = FastAPI()
 user_id = "minju"
 
 @app.get("/users/{user_id}")
-async def read_user(user_id: int, q: str = None):
+def read_user(user_id: int, q: str = None):
     return {"user_id": user_id, "q": q}
 
 
 @app.post("/users/")
-async def create_user(q: str = None):
+def create_user(q: str = None):
     return {"q": q}
 
 @app.put("/users/{user_id}")
-async def update_user(user_id: int, q: str = None):
+def update_user(user_id: int, q: str = None):
     return {"user_id": user_id, "q": q}
 
-@app.delete("/users/{user_id}")
-async def delete_user(user_id: int):
-    return {"user_id": user_id}
+@app.delete("/users/")
+def delete_user():
+    user_id = ""
+    return {"message": "User has been deleted"}
