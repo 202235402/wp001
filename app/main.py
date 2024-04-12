c60ac5ae7a094006a5b234a28b1953f6404b872e
minju kang <kmj20030421@gmail.com>
2024-04-12 17:05:22 +0900

    init

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 from typing import Union
+from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+user_id = "minju"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/users/{user_id}")
+async def read_user(user_id: int, q: str = None):
+    return {"user_id": user_id, "q": q}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/users/")
+async def create_user(q: str = None):
+    return {"q": q}
+
+@app.put("/users/{user_id}")
+async def update_user(user_id: int, q: str = None):
+    return {"user_id": user_id, "q": q}
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    return {"user_id": user_id}
