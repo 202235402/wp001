d32ef9497ddf4ec64d1877f3650f9aecbb7e45f4
minju kang <kmj20030421@gmail.com>
2024-04-12 17:26:37 +0900

    init

--- app/main.py
+++ app/main.py
@@ -8,3 +8,17 @@ user_name = "minju"
 @app.get("/")
 def read_name():
     return {"name": user_name}
+
+@app.post("/")
+def create_name():
+
+    return {}
+
+@app.put("/")
+def update_name():
+
+    return {}
+
+@app.delete("/")
+def delete_name():
+    return {"name": ""}
