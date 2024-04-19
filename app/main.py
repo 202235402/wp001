0d2306c9a5ca5e044b130b696463e6094e70f970
minju kang <kmj20030421@gmail.com>
2024-04-19 16:13:13 +0900

    openapi

--- app/main.py
+++ app/main.py
@@ -4,13 +4,11 @@ import requests
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=xxx&returnType=json"
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2019&month=4&metroCd=31&cityCd=102220&bizCd=C&apiKey=8U23O1wm7905rkrBzb5z0NCpQPIMPqoUCJA22Bw9&returnType=json"
   
-    contents = request.get(URL).text
+    contents = requests.get(URL).text
 
     return { "message": contents }
-@app.get("/home")
-def home():
-    return { "message": "Home!" }
