35bf4ac04603c7e63d7e62373ba51140278d0166
minju kang <kmj20030421@gmail.com>
2024-05-10 01:26:07 +0900

    init

--- app/main.py
+++ app/main.py
@@ -1,55 +1,29 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, HTTPException
+import requests
 from pydantic import BaseModel
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///../todo.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 app = FastAPI()
 
-class Todo(Base):
-    __tablename__ = "todos"
-    __allow_unmapped__ = True
+class Vaccine(BaseModel):
+    ServiceKey: str
+    PRDLIST_SN: str
+    pageNo: int = 1
+    numOfRows: int = 10
+    type: str
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
 
-Base.metadata.create_all(bind=engine)
+@app.post("/vaccine/")
+def fetch_vaccine_info(query: Vaccine):
+    URL = "http://apis.data.go.kr/1471000/IcdVacinDrugPrdtInfoService/getIcdVacinDrugPrdtInfo"
+    prm = {
+        'ServiceKey': query.ServiceKey,
+        'PRDLST_SN': query.PRDLST_SN,
+        'pageNo': query.pageNo,
+        'numOfRows': query.numOfRows,
+        'type': query.type
+    }
+    contents = requests.get(URL, prm=prm)
+    if contents.status.code != 200:
+        raise HTTPException(status_code=404, detail="API request failed")
 
-class TodoCreate(BaseModel):
-    title: str
-    description: str
-
-class TodoUpdate(BaseModel):
-    title: str
-    description: str
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally: 
-        db.close()
-
-@app.post("/todos/")
-def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
-    db_todo = Todo(title=todo.title, description=todo.description)
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
-
-@app.get("/todos/{todo_id}")
-def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if db_todo is None:
-        raise HTTPException(status_code=404, detail="todo not found")
-    return db_todo
-
+    return {"message": contents}
